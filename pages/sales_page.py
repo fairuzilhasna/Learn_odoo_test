dea34deed448b5398eac97a415af60c905f6cdf0
Fairuzil Hasna <fairuzilhasna09@gmail.com>
2025-01-31 15:47:39 +0700

    pom

--- pages/sales_page.py
+++ pages/sales_page.py
@@ -1,27 +1,160 @@
-from selenium.webdriver.common.by import By
-from .base_page import BasePage
+from selenium.webdriver.common.by import By # mengimpor modul dari selenium yang digunakan untuk menemukan elemen elemen dalam halaman web
+from .base_page import BasePage # mengimpor kelas base_page.py yang berada dalam direktori yang samaS
 
 
 class SalesPage(BasePage):
-    New_button = (By.XPATH, "/html/body/div[1]/div/div[1]/div[2]/div[1]/div/div/button[1]")
-    Customer_name = (By.ID, "partner_id")
-    Create_button = (By.ID, "partner_id_0_0")
-    Save_button = (By.XPATH, "/html/body/div[1]/div/div/div[1]/div/div[1]/ol/li[2]/div/div/button[1]/i")
-    Message_bubble = (By.XPATH, "/html/body/div[1]/div/div[2]/div/div/div[2]/div/div/div/div/div[2]/div[2]/div[2]/div[2]/div[1]/div[2]/div/p")
+    New_button = (By.XPATH, "/html/body/div[1]/div/div[1]/div[2]/div[1]/div/div/button[1]") # lokator elemen tombol "New"
+    Customer_name = (By.ID, "partner_id") # lokator elemen input "Customer Name"
+    Create_button = (By.ID, "partner_id_0_0") # lokator elemen tombol "Create"
+    Save_button = (By.XPATH, "/html/body/div[1]/div/div/div[1]/div/div[1]/ol/li[2]/div/div/button[1]/i") # lokator elemen tombol "Save"
+    Sales_navbar = (By.XPATH, "/html/body/header/nav/a") # lokator elemen navbar "Sales"
+    Status = (By.XPATH, "/html/body/div[1]/div/div[3]/div/table/tbody/tr[1]/td[9]/div/span") # lokator elemen "Status"
+    Message_bubble = (By.XPATH, "/html/body/div[1]/div/div[2]/div/div/div[2]/div/div/div/div/div[2]/div[2]/div[2]/div[2]/div[1]/div[2]/div/p") # lokator elemen "Message Bubble"
+    Customer = (By.XPATH, "/html/body/div[1]/div/div[3]/div/table/tbody/tr[1]/td[5]/div/div/span[2]/span") # lokator elemen "Customer"
+    Send_mail = (By.XPATH, "/html/body/div[1]/div/div[1]/div[2]/div/div/div[1]/div[1]/button[1]") # lokator elemen "Send Mail"
+    Email = (By.ID, "email") # lokator elemen input "Email"
+    Save_and_close_button = (By.XPATH, "/html/body/div[2]/div[5]/div/div[2]/div/div/div/footer/div/div/button[1]") # lokator elemen tombol "Save & Close"
+    Send_button = (By.XPATH, "/html/body/div[2]/div[5]/div/div/div/div/div/footer/footer/button[1]") # lokator elemen tombol "Send"
+    Message_bubble_2 = (By.XPATH, "/html/body/div[1]/div/div[2]/div/div/div[2]/div/div/div/div/div[2]/div[2]/div[2]/div[2]/div[1]/div[1]") # lokator elemen "Message Bubble" ke dua
+    Confirm_order_button = (By.ID, "action_confirm") # lokator elemen tombol "Confirm"
+    Message_bubble3 = (By.XPATH, "/html/body/div[1]/div/div[2]/div/div/div[2]/div/div/div/div/div[2]/div[2]/div[2]/div[2]/div[1]/div/p") # lokator elemen "Message Bubble" ke tiga
+    Invoice_button = (By.ID, "create_invoice_percentage") # lokator elemen tombol "Invoice"
+    Down_payment_amount = (By.ID, "amount") # lokator elemen input "Down Payment Amount"
+    Create_invoice_button = (By.ID, "create_invoice_open") # lokator elemen tombol "Create Invoice"
+    Message_bubble4 = (By.XPATH, "/html/body/div[1]/div/div[2]/div/div/div[2]/div/div/div/div/div[3]/div[2]/div[2]/div[2]/div[1]/div/p") # lokator elemen "Message Bubble" ke empat
+    Allert = (By.XPATH, "/html/body/div[2]/div[6]/div/strong") # lokator elemen "Allert"
+    Cancel_button = (By.XPATH, "/html/body/div[1]/div/div[1]/div[2]/div/div/div[1]/div[1]/button[2]/span") # lokator elemen tombol "Cancel"
+    Cancel_button2 = (By.XPATH, "//*[@id='dialog_6']/div/div/div/footer/footer/button[2]/span") # lokator elemen tombol "Cancel" ke dua
+    Status_canceled = (By.XPATH, "/html/body/div[1]/div/div[2]/div/div/div[2]/div/div/div/div/div[2]/div[2]/div[2]/div[2]/div[1]/div/ul/li/div/span[2]") # lokator elemen "Status" yang telah dibatalkan
+    Confirm_button = (By.XPATH, "/html/body/div[1]/div/div[1]/div[2]/div/div/div[1]/div[1]/button[2]/span") # lokator elemen tombol "Confirm"
+    Message_bubble5 = (By.XPATH, "/html/body/div[1]/div/div[2]/div/div/div[2]/div/div/div/div/div[2]/div[2]/div[2]/div[2]/div[1]/div/p") # lokator elemen tombol "Confirm" ke lima
+    Action_button = (By.XPATH, "/html/body/div[1]/div/div[1]/div[1]/div/div[2]/div[1]/div[2]/button/span") # lokator elemen tombol "Action"
+    Delete_button = (By.XPATH, "/html/body/div[1]/div/div[1]/div[1]/div/div[2]/div[1]/div[2]/div/span[2]") # lokator elemen tombol "Delete"
+    Confirm_delete_button = (By.XPATH, "//*[@id='dialog_44']/div/div/div/footer/button[1]") # lokator elemen tombol "Confirm" delete
+    User_error_delete = (By.XPATH, "//*[@id='dialog_45']/div/div/div/main/div/p") # lokator elemen User Error delete
+    Ok_button_error_delete = (By.XPATH, "//*[@id='dialog_45']/div/div/div/footer/button") # lokator elemen tombol "OK" error delete
+    Add_product = (By.XPATH, "/html/body/div[1]/div/div[1]/div[2]/div/div/div[2]/div[4]/div[2]/div/div[1]/div/div[2]/table/tbody/tr[1]/td[2]/a[1]") # lokator elemen tombol "Add Product"
+    Dropdown_product = (By.XPATH, "/html/body/div[1]/div/div[1]/div[2]/div/div/div[2]/div[4]/div[2]/div/div[1]/div/div[2]/table/tbody/tr[1]/td[2]/div/div[1]/div/a") # lokator elemen tombol "Drop Product"
+    Sample_product = (By.ID, "autocomplete_0_6") # lokator elemen "Sample Product"
+    Message_bubble6 = (By.XPATH, "/html/body/div[1]/div/div[2]/div/div/div[2]/div/div/div/div/div[3]/div[2]/div[2]/div[2]/div[1]/div[2]/div/p")
 
 
-    def click_new_button (self):
-        self.click (self.New_button)
+    def click_new_button (self): # Fungsi untuk mengklik tombol "New"
+        self.click (self.New_button) # Memanggil metode click untuk mengklik tombol "New"
 
-    def input_customer_name (self, name):
-        self.enter_text (self.Customer_name, name)
+    def input_customer_name (self, name): # Fungsi untuk memasukkan nama customer ke input "Customer Name"
+        self.enter_text (self.Customer_name, name) # Memanggil metode enter_text untuk memasukkan teks ke input "Customer Name"
 
-    def click_create_button (self):
-        self.click (self.Create_button)
+    def click_create_button (self): # Fungsi untuk mengklik tombol "Create"
+        self.click (self.Create_button) # Memanggil metode click untuk mengklik tombol "Create"
 
-    def get_message_bubble (self):
-        element = self.find_element(self.Message_bubble)
-        return element.text
+    def get_message_bubble (self): # Fungsi untuk mendapatkan teks "Message Bubble"
+        element = self.find_element(self.Message_bubble) # Memanggil metode find_element untuk mencari elemen "Message Bubble"
+        return element.text # Mengembalikan teks "Message Bubble"
 
-    def click_save_button (self):
-        self.click (self.Save_button)
+    def click_save_button (self): # Fungsi untuk mengklik tombol "Save"
+        self.click (self.Save_button) # Memanggil metode click untuk mengklik tombol "Save"
+
+    def click_sales_navbar (self): # Fungsi untuk mengklik tombol "Sales"
+        self.click (self.Sales_navbar) # Memanggil metode click untuk mengklik tombol "Sales"
+
+    def get_status (self): # Fungsi untuk mendapatkan teks "Status"
+        element = self.find_element(self.Status) # Memanggil metode find_element untuk mencari elemen "Status"
+        return element.text # Mengembalikan teks "Status"
+    
+    def click_customer (self): # Fungsi untuk mengklik tombol "Customer"
+        self.click (self.Customer) # Memanggil metode click untuk mengklik tombol "Customer"
+
+    def click_send_mail (self): # Fungsi untuk mengklik tombol "Send Mail"
+        self.click (self.Send_mail) # Memanggil metode click untuk mengklik tombol "Send Mail"
+
+    def input_email (self, email): # Fungsi untuk memasukkan email ke input email
+        self.enter_text (self.Email, email) # Memanggil metode enter_text untuk memasukkan teks ke input email
+
+    def click_save_and_close_button (self): # Fungsi untuk mengklik tombol "Save and Close"
+        tried = 1;
+        while tried <= 5:
+            print(tried)
+            element = self.find_element(self.Save_and_close_button)
+            print('2')
+            if element == True:
+                print(3)
+                self.click (self.Save_and_close_button) # Memanggil metode click untuk mengklik tombol "Save and Close"
+                print(4)
+                tried++1
+            else:
+                print(5)
+                tried++1
+
+    def click_send_button (self): # Fungsi untuk mengklik tombol "Send"
+        self.click (self.Send_button) # Memanggil metode click untuk mengklik tombol "Send"
+
+    def get_bubble_message2 (self): # Fungsi untuk mendapatkan teks "Message Bubble" ke dua
+        element = self.find_element(self.Message_bubble_2) # Memanggil metode find_element untuk mencari elemen "Message Bubble"
+        return element # Mengembalikan elemen "Message Bubble"
+    
+    def click_confirm_order_button (self): # Fungsi untuk mengklik tombol "Confirm"
+        self.click (self.Confirm_order_button) # Memanggil metode click untuk mengklik tombol "Confirm"
+
+    def get_message_bubble3 (self): # Fungsi untuk mendapatkan teks "Message Bubble"    
+        element = self.find_element(self.Message_bubble3) # Memanggil metode find_element untuk mencari elemen "Message Bubble"
+        return element.text # Mengembalikan teks "Message Bubble"
+    
+    def click_invoice_button (self): # Fungsi untuk mengklik tombol "Invoice"
+        self.click (self.Invoice_button) # Memanggil metode click untuk mengklik tombol "Invoice"
+
+    def click_create_invoice_button (self): # Fungsi untuk mengklik tombol "Create Invoice"
+        self.click (self.Create_invoice_button) # Memanggil metode click untuk mengklik tombol "Create Invoice"
+
+    def get_message_bubble4 (self): # Fungsi untuk mendapatkan teks "Message Bubble"
+        element = self.find_element(self.Message_bubble4) # Memanggil metode find_element untuk mencari elemen "Message Bubble"
+        return element.text # Mengembalikan teks "Message Bubble"
+    
+    def allert_message (self): # Fungsi untuk mendapatkan teks "Allert"
+        element = self.find_element(self.Allert) # Memanggil metode find_element untuk mencari elemen "Allert"
+        return element.text # Mengembalikan teks "Allert"
+    
+    def click_cancel_button (self): # Fungsi untuk mengklik tombol "Cancel"
+        self.click (self.Cancel_button) # Memanggil metode click untuk mengklik tombol "Cancel"
+
+    def click_cancel_button2 (self): # Fungsi untuk mengklik tombol "Cancel"
+        self.click (self.Cancel_button2) # Memanggil metode click untuk mengklik tombol "Cancel"
+
+    def get_status_canceled (self): # Fungsi untuk mendapatkan teks "Status Canceled"
+        element = self.find_element(self.Status_canceled) # Memanggil metode find_element untuk mencari elemen "Status Canceled"
+        return element.text # Mengembalikan teks "Status Canceled"
+    
+    def click_confirm_button (self): # Fungsi untuk mengklik tombol "Confirm"
+        self.click (self.Confirm_button) # Memanggil metode click untuk mengklik tombol "Confirm"
+
+    def get_message_bubble5 (self): # Fungsi untuk mendapatkan teks "Message Bubble"
+        element = self.find_element(self.Message_bubble5) # Memanggil metode find_element untuk mencari elemen "Message Bubble"
+        return element.text # Mengembalikan teks "Message Bubble"
+    
+    def click_action_button (self): # Fungsi untuk mengklik tombol "Action"
+        self.click (self.Action_button) # Memanggil metode click untuk mengklik tombol "Action"
+
+    def click_delete_button (self): # Fungsi untuk mengklik tombol "Delete"
+        self.click (self.Delete_button) # Memanggil metode click untuk mengklik tombol "Delete"
+
+    def click_confirm_delete_button (self): # Fungsi untuk mengklik tombol "Confirm"
+        self.click (self.Confirm_delete_button) # Memanggil metode click untuk mengklik tombol "Confirm"
+
+    def get_user_error_delete (self): # Fungsi untuk mendapatkan teks "User Error Delete"
+        element = self.find_element(self.User_error_delete) # Memanggil metode find_element untuk mencari elemen "User Error Delete"
+        return element.text # Mengembalikan teks "User Error Delete"
+    
+    def click_ok_button_delete (self): # Fungsi untuk mengklik tombol "OK"
+        self.click (self.Ok_button_error_delete) # Memanggil metode click untuk mengklik tombol "OK"
+
+    def click_add_product (self): # Fungsi untuk mengklik tombol "Add Product"
+        self.click (self.Add_product) # Memanggil metode click untuk mengklik tombol "Add Product"
+
+    def click_Dropdown_product (self): # Fungsi untuk mengklik tombol "Drop Product"
+        self.click (self.Dropdown_product) # Memanggil metode click untuk mengklik tombol "Drop Product"
+
+    def click_sample_product (self): # Fungsi untuk mengklik tombol "Sample Product"
+        self.click (self.Sample_product) # Memanggil metode click untuk mengklik tombol "Sample Product"
+
+    def get_message_bubble6 (self): # 
+        self.click (self.Message_bubble6) # 
--- tests/sales_test.py
+++ tests/sales_test.py
@@ -1,53 +1,267 @@
-from selenium import webdriver
-from pages.login_page import LoginPage
-from pages.sales_page import SalesPage
+from selenium import webdriver # mengimpor modul selenium
+from selenium.webdriver.common.by import By # mengimpor modul dari selenium yang digunakan untuk menemukan elemen elemen dalam halaman web
+from selenium.common.exceptions import TimeoutException # Mengimpor TimeoutException dari Selenium yang digunakan untuk menangani situasi saat Selenium menunggu lebih lama dari waktu yang ditentukan dan elemen yang dicari tidak ditemukan
+from selenium.webdriver.support.ui import WebDriverWait # Mengimpor WebDriverWait yang digunakan untuk menunggu sampai kondisi tertentu tercapai dalam pengujian atau operasi di halaman web. Berguna saat elemen membutuhkan waktu untuk muncul atau berubah status
+from selenium.webdriver.support import expected_conditions as EC # Mengimpor expected_conditions yang digunakan untuk menentukan kondisi yang diharapkan dalam pengujian atau operasi di halaman web
+from pages.login_page import LoginPage # Mengimpor kelas LoginPage dari modul login_page yang ada di folder pages. Digunakan untuk melakukan operasi pada halaman login
+from pages.sales_page import SalesPage # Mengimpor kelas SalesPage dari modul sales_page yang ada di folder pages. Digunakan untuk melakukan operasi pada halaman sales
 import time
 
 
 # Inisialisasi WebDriver
-driver = webdriver.Chrome()
-driver.maximize_window()
+driver = webdriver.Chrome() # Menggunakan Chrome sebagai browser
+driver.maximize_window() # Maksimalkan ukuran jendela browser
 
 
-def login(driver):
+def login(driver): # Mendefinisikan fungsi login yang menerima parameter driver. Fungsi ini akan melakukan proses login ke aplikasi web yang diuji.
     # URL
-    baseURL = "http://localhost:8116/"
+    baseURL = "http://localhost:8116/" # Mendefinisikan URL dasar untuk aplikasi yang akan diakses
 
-    driver.get (baseURL)
-    login_page = LoginPage(driver)
+    driver.get (baseURL) # Membuka halaman yang ditentukan dalam variabel baseURL di browser yang dikelola oleh Selenium
+    login_page = LoginPage(driver) # Membuat objek login_page yang merupakan instance dari kelas LoginPage
 
-    login_page.input_email('admin')
-    login_page.input_password('admin')
-    login_page.click_Login()
+    login_page.input_email('admin') # Memanggil metode input_email dari objek login_page untuk memasukkan email "admin"
+    login_page.input_password('admin') # Memanggil metode input_password dari objek login_page untuk memasukkan password "admin"
+    login_page.click_Login() # Memanggil metode click_Login dari objek login_page untuk mengklik tombol login
 
-    login_page.click_home_menu()
-    login_page.click_sales_menu()
+    login_page.click_home_menu() # Memanggil metode click_home_menu dari objek login_page untuk mengklik tombol menu home
+    login_page.click_sales_menu() # Memanggil metode click_sales_menu dari objek login_page untuk mengklik tombol menu sales
 
-    time.sleep(2)
+    time.sleep(2) # Menunggu selama 2 detik
 
 # S.01.001 User membuat quotation baru
-def new_quotation():
-    login(driver)
-    sales_page = SalesPage(driver)
+def New_quotation(): # Fungsi untuk membuat quotation baru
+    login(driver) # Memanggil fungsi login
+    sales_page = SalesPage(driver) # Membuat objek sales_page yang merupakan instance dari kelas SalesPage
 
-    sales_page.click_new_button()
-    sales_page.input_customer_name('Sample')
-    sales_page.click_create_button()
-    sales_page.click_save_button()
-    message = sales_page.get_message_bubble()
-    print (message)
+    sales_page.click_new_button() # Memanggil metode click_new_button dari objek sales_page untuk mengklik tombol "New"
+    sales_page.input_customer_name('ghsdfak') # Memanggil metode input_customer_name dari objek sales_page untuk memasukkan nama customer "ghsdfak"
+    sales_page.click_create_button() # Memanggil metode click_create_button dari objek sales_page untuk mengklik tombol "Create"
+    sales_page.click_save_button() # Memanggil metode click_save_button dari objek sales_page untuk mengklik tombol "Save"
+
+
+    message = sales_page.get_message_bubble() # Memanggil metode get_message_bubble dari objek sales_page untuk mendapatkan teks Message Bubble
+    print (message) # Menampilkan teks Message Bubble
 
     time.sleep(2)
 
+    try: # blok kode yang digunakan untuk menangani pengecualian (exception). Kode yang berpotensi menyebabkan error di tulis di dalam blok try. Jika ada error, program akan berpindah ke blok except yang sesuai.
+        # Tujuan dari penggunaan try adalah untuk menangkap dan menangani kesalahan yang mungkin terjadi saat eksekusi kode, agar program tidak langsung berhenti (crash) ketika error ditemukan.
+        assert message == "Sales Order created" # statement untuk mengecek apakah suatu kondisi bernilai benar. Jika benar, program akan melanjutkan eksekusi ke baris berikutnya.
+        # Jika kondisi bernilai salah, program akan melanjutkan eksekusi ke baris except
+        print ("New quotation successed") # Jika kondisi di atas benar, maka progranm akan menampilkan pesan "New quotation successed"
+    except AssertionError: #bagian yang menangani error AssertionError. Jika assert gagal, maka Python akan melemparkan AssertionError, dan kode di dalam blok except ini akan dijalankan
+        print ("New quotation failed") # Jika kondisi di atas salah, maka program akan menampilkan pesan "New quotation failed"
+
+    sales_page.click_sales_navbar() # Memanggil metode click_sales_navbar dari objek sales_page untuk mengklik tombol navbar Sales
+    status = sales_page.get_status() # Memanggil metode get_status dari objek sales_page untuk mendapatkan teks Status
+    print (status) # Menampilkan teks Status
+
     try:
-        assert message == "Sales Order created"
+        assert status == "Quotation" 
         print ("New quotation successed")
     except AssertionError:
         print ("New quotation failed")
+
+# S.01.002 User mengirim quotation
+def Send_quotation(): # Fungsi untuk mengirim quotation
+    sales_page = SalesPage(driver) # Membuat objek sales_page yang merupakan instance dari kelas SalesPage
+
+    sales_page.click_customer() # Memanggil metode click_customer dari objek sales_page untuk mengklik nama Customer
+    sales_page.click_send_mail() # Memanggil metode click_send_mail dari objek sales_page untuk mengklik tombol "Send Mail"
+    time.sleep(2)
+    sales_page.input_email('test@test.com') # Memanggil metode input_email dari objek sales_page untuk memasukkan email "test@test.com"
+    sales_page.click_save_and_close_button() # Memanggil metode click_save_and_close_button dari objek sales_page untuk mengklik tombol "Save and Close"
+
+    # Tunggu hingga modal hilang sebelum mengklik tombol Kirim
+    try:
+        WebDriverWait(driver, 10).until(
+            EC.invisibility_of_element_located((By.CLASS_NAME, "o_technical_modal"))
+        )
+    except TimeoutException:
+        print("Modal masih terlihat setelah menunggu")
+
+    # Coba klik tombol Kirim jika tidak terhalang
+    try:
+        WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.CLASS_NAME, "btn btn-primary o_mail_send"))
+        )
+        sales_page.click_send_button()
+        print("Tombol Kirim diklik")
+    except TimeoutException:
+        print("Tombol Kirim masih tidak bisa diklik setelah menunggu")
+
+    time.sleep(2)
     
+    message_element = sales_page.get_bubble_message2()
+    print (message_element)
+    
+    try:
+        assert message_element.is_displayed(), "Message bubble not displayed"
+        print ("Send quotation successed")
+    except AssertionError:
+        print ("Send quotation error")
+
+    sales_page.click_sales_navbar()
+    status = sales_page.get_status()
+    print (status)
+
+    try:
+        assert status == "Quotation sent" 
+        print ("Send quotation successed")
+    except AssertionError:
+        print ("Send quotation error")
+
+# S.01.003 User mengkonfirmasi orderan
+def Confirm_order():
+    sales_page = SalesPage(driver)
+
+    sales_page.click_confirm_order_button()
+    message = sales_page.get_message_bubble3()
+    print (message)
+
+    try:
+        assert message == "Quotation confirmed" 
+        print ("Confirm order successed")
+    except AssertionError:
+        print ("Confirm order failed") 
+
+    sales_page.click_sales_navbar()
+    status = sales_page.get_status()
+    print (status)
+
+    try:
+        assert status == "Sales order" 
+        print ("Confirm order successed")
+    except AssertionError:
+        print ("Confirm order successed")
+    
+# S.01.004 User menerbitkan invoice
+def Create_invoice():
+    sales_page = SalesPage(driver)
+
+    sales_page.click_invoice_button()
+    sales_page.input_down_payment_amount('1')
+    sales_page.click_create_invoice_button()
+
+    sales_page.click_confirm_button()
+    message = sales_page.get_message_bubble4()
+    print (message)
+
+    try:
+        assert message == "Invoice Created" 
+        print ("Create invoice successed")
+    except AssertionError:
+        print ("Create invoice failed")
+
+# S.01.005 User membuat quotation baru dengan data kosong
+def No_data_quotation():
+    sales_page = SalesPage(driver)
+
+    sales_page.click_sales_navbar()
+    sales_page.click_new_button()
+    sales_page.click_save_button()
+
+    time.sleep(2)
+
+    allert = sales_page.allert_message()
+    print (allert)
+
+    try:
+        assert allert == "Invalid fields: " 
+        print ("New quotation successed")
+    except AssertionError:
+        print ("New quotation failed")
+
+# S.01.006 User membatalkan quotation
+def Cancel_quotation():
+    sales_page = SalesPage(driver)
+
+    sales_page.click_sales_navbar()
+    sales_page.click_customer()
+    sales_page.click_cancel_button()
+    #sales_page.click_cancel_button2()
+    message = sales_page.get_status_canceled()
+    print (message)
+
+    try:
+        assert message == "Cancelled" 
+        print ("Cancel quotation successed")
+    except AssertionError:
+        print ("Cancel quotation failed")
+
+    sales_page.click_sales_navbar()
+    status = sales_page.get_status()
+    print (status)
+
+    try:
+        assert status == "Cancelled" 
+        print ("Cancel quotation successed")
+    except AssertionError:
+        print ("Cancel quotation failed")
+
+ # S.01.007 User mengkonfirmasi orderan
+def Confirm_order():
+    sales_page = SalesPage(driver)
+
+    sales_page.click_sales_navbar()
+    sales_page.click_new_button()
+    sales_page.input_customer_name('anita')
+    sales_page.click_create_button()
+    sales_page.click_confirm_button()
+
+    message = sales_page.get_message_bubble5()
+    print (message)
+
+    try:
+        assert message == "Quotation confirmed" 
+        print ("Confirm order successed")
+    except AssertionError:
+        print ("Confirm order failed") 
+
+    sales_page.click_sales_navbar()
+    status = sales_page.get_status()
+    print (status)
+
+    try:
+        assert status == "Sales order" 
+        print ("Confirm order successed")
+    except AssertionError:
+        print ("Confirm order successed")
+
+# S.01.008 User menghapus orderan
+def Delete_order():
+    sales_page = SalesPage(driver)
+
+    sales_page.click_sales_navbar()
+    sales_page.click_customer()
+    sales_page.click_action_button()
+    sales_page.click_delete_button()
+    time.sleep(2)
+    sales_page.click_confirm_delete_button()
+
+    message = sales_page.get_user_error_delete()
+    print (message)
+
+    try:
+        assert message == "You can not delete a sent quotation or a confirmed sales order. You must first cancel it." 
+        print ("Delete order successed")
+    except AssertionError:
+        print ("Delete order failed")
+
+    sales_page.click_ok_button_delete()
+
 
 try:
-    new_quotation()
+    New_quotation()
+    Send_quotation()
+    Confirm_order()
+    Create_invoice()
+    No_data_quotation()
+    Cancel_quotation()
+    Confirm_order()
+    Delete_order()
+
 
 finally:
     driver.quit()
